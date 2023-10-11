64b9f1aee52c770a6282692857de97f56f448a0d
anisa2472 <anisa.psari24@gmail.com>
2023-10-11 11:18:21 +0700

    update: remove logo outline

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -9,24 +9,16 @@ const Logo = ({ isDarkTheme }) => {
         href='/'
         className='relative w-10 h-10 flex justify-center items-center'
       >
-        {isDarkTheme ? (
-          <Image
-            src='/SakuraFill.svg'
-            width={40}
-            height={40}
-            alt='Anisa Permatasari logo'
-            className='absolute -z-10 top-0 left-0'
-          />
-        ) : (
-          <Image
-            src='/SakuraRegular.svg'
-            width={40}
-            height={40}
-            alt='Anisa Permatasari logo'
-            className='absolute -z-10 top-0 left-0'
-          />
-        )}
-        <span className='font-bold text-primary-500 dark:text-primary-100'>A</span>
+        <Image
+          src='/SakuraFill.svg'
+          width={40}
+          height={40}
+          alt='Anisa Permatasari logo'
+          className='absolute -z-10 top-0 left-0'
+        />
+        <span className='font-bold text-neutral-100'>
+          A
+        </span>
       </Link>
     </div>
   );
